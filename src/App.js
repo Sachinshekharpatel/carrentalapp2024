d053f4c70a1ab5436426ce0e3a6d0075ad211bad
Sachinshekharpatel <heroft7024@gmail.com>
2024-05-23 17:29:17 +0530

    improve some functionalityevrything is working payment gateway using razorpaytestmode

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LoginPage from "./Components/login-page/loginpage";
 import WelcomePage from "./Components/welcome-page/welcomepage";
 import ContactPage from "./Components/contactpage/contactus";
 import BookingPage from "./Components/booking-page/bookingpage";
+import ChatBox from "./Components/chatboxpage/chatbox";
 const App = () => {
   return (
     <Router>
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/loginpage" element={<LoginPage />} />
         <Route path="/signuppage" element={<SignupPage />} />
         <Route path="/contactuspage" element={<ContactPage />} />
+        <Route path="/chatbox" element={<ChatBox />} />
       </Routes>
     </Router>
   );
